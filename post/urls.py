e3a2b4cbbdb8deb29086dcc5fba6fa3b177a699c
chanhyoYun <chloeee2@naver.com>
2023-04-11 20:15:12 +0900

    포스트 상세페이지 추가

--- post/urls.py
+++ post/urls.py
@@ -7,8 +7,7 @@ app_name = 'post'
 urlpatterns = [
     path('', views.post_views, name='postMain'),
     path('write-post/', views.write_post, name='write-post'),
-    path('post-detail/', views.post_detail, name='post-detail'),
+    path('post-detail/<int:id>', views.post_detail, name='post-detail'),
     path('delete-post/', views.delete_post, name='delete-post'),
     path('edit-post/', views.edit_post, name='edit-post'),
-
 ]
--- post/views.py
+++ post/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from user.models import User as user_model
 
 # Create your views here.
 
 
 
 def post_views(request):
-    """전체 게시글을 보여주는 함수"""
-    return render(request, 'post/main.html')
+    user = request.user.is_authenticated
+    if user:
+        post_list = Post.objects.order_by('caption')
+        return render(request, 'post/main.html', {'posts': post_list})
 
 
-def post_detail(request):
-    """게시글 상세보기"""
-    pass
+def post_detail(request, id):
+    user = get_object_or_404(user_model, id=request.user.id)
+    my_post = Post.objects.get(id=user.id)
+    return render(request, 'post/post-detail.html', {'posts': my_post})
 
 
 def write_post(request):
@@ -30,3 +35,11 @@ def edit_post(request):
 
 def delete_post(request):
     """게시글을 삭제하는 함수"""
+    pass
+
+
+def post_list(request):
+    user = request.user.is_authenticated
+    if user:
+        post_list = Post.objects
+        return render(request, 'post/main.html', {'posts': post_list})
