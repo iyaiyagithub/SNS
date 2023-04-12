5710ff620ab0e9483fdfb291865f5a89da65311c
Jo Hye Min <reviewmine@naver.com>
2023-04-12 20:48:39 +0900

    수정삭제함수미완유알엘

--- post/urls.py
+++ post/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.post_views, name='postMain'),
     path('write-post/', views.write_post, name='write-post'),
     path('post-detail/<int:id>', views.post_detail, name='post-detail'),
-    path('delete-post/', views.delete_post, name='delete-post'),
-    path('edit-post/', views.edit_post, name='edit-post'),
+    path('delete-post/<int:id>', views.delete_post, name='delete-post'),
+    path('edit-post/<int:id>', views.edit_post, name='edit-post'),
 ]
