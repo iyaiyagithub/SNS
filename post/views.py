bbeb3780a33d3158699b927037b4d71ff00ed0c3
minjeong <ehdro418@naver.com>
2023-04-14 03:50:49 +0900

    게시글 수정 시 해당 게시글의 detail 페이지로 이동

--- post/views.py
+++ post/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
-from .models import Post
-from user.models import User as user_model
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
+from .models import Post
+from user.models import User as user_model
 
 # Create your views here.
 
@@ -19,9 +18,11 @@ def post_views(request):
 
 
 def post_detail(request, id):
-    user = get_object_or_404(user_model, id=request.user.id)
-    my_post = Post.objects.get(id=user.id)
-    return render(request, 'post/post-detail.html', {'posts': my_post})
+    post = get_object_or_404(Post, id=id)
+    context = {
+        'post': post,
+    }
+    return render(request, 'post/post-detail.html', context)
 
 
 @login_required(login_url='')
@@ -50,22 +51,32 @@ def edit_post(request, id):
     current_edit_post = edit_post.id
 
     if request.method == 'GET':
-        return render(request, 'post/write-post.html', {'edit-post': edit_post})
+        edit_form = PostForm(instance=edit_post)
+        context = {
+            'form': edit_form,
+            'edit': '수정하기',
+        }
+        return render(request, 'post/write-post.html', context)
 
     elif request.method == 'POST':
-        edit_post.caption = request.POST.get("edit_post_caption", "")
-        edit_post.image = request.POST.get("edit_post_image", "")
-        edit_post.save()
-        return redirect('/edit-post/'+str(current_edit_post))
+        user = get_object_or_404(user_model, pk=request.user.id)
+        edit_form = PostForm(request.POST, request.FILES, instance=edit_post)
+        if edit_form.is_valid():
+            edit_post = edit_form.save(commit=False)
+            edit_post.author = user
+            edit_post.id = current_edit_post
+            edit_post.save()
+            return redirect('/post/post-detail/'+str(current_edit_post))
 
 
 @login_required(login_url='')
 def delete_post(request, id):
     """게시글을 삭제하는 함수"""
     delete_post = Post.objects.get(id=id)
-    current_delete_post = delete_post.id
+    # current_delete_post = delete_post.id
     delete_post.delete()
-    return redirect('/delete-post/'+str(current_delete_post))
+    # return redirect('/delete-post/'+str(current_delete_post))
+    return redirect('/post')
 
 
 """피드 페이지 """
