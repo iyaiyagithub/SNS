fc6a8bcfbf9a9f6b9be2cba0bfdba6ba8eefbe4f
minjeong <ehdro418@naver.com>
2023-04-13 11:35:16 +0900

    프로필 사진/게시글 작성 에러 수정

--- post/forms.py
+++ post/forms.py
@@ -7,4 +7,3 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post  # model은 Post 양식으로 쓰겠다.
         fields = ('caption', 'image')  # 어떤 필드를 입력 받을 지
-        exclude = ('author', )  # 폼에서 author 입력받지 않게 함
